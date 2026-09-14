ca0a7479ce7e1592c884af16a1649dfefcca97cc
PanasenkoDiana <shad0wdrag0n19902@gmail.com>
2026-09-14 18:40:15 +0300

    clear index.js to standart server settings

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 const PORT = 8000
-
 const HOST = 'localhost'; 
 
 app.get('/', (req, res)=>{
@@ -11,28 +10,6 @@ app.get('/', (req, res)=>{
     res.json('text')
 }
 )
-app.get('/Valera', (req, res)=>{
-    res.json({
-        name: "Valera",
-        age: 15,
-        hobby: "ne delyat domashnee zadanye"
-    })
-})
-app.get('/Ilya', (req, res) => {
-    res.json({
-        name: "Ilya",
-        age: "16",
-        hobby: "hobby"
-    })
-})
-
-app.get('/Arina', (req,res)=>{
-    res.json({
-        name: "Arina",
-        age: 16,
-        hobby: "12345678"
-    })
-})
 
 app.listen(PORT, HOST, ()=>{
     console.log(`Сервер запущен на http://${HOST}:${PORT}`)
