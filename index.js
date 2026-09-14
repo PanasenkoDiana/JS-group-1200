69dacecb74f632e351b41466726bb5b5d4a513d4
MaximAndreev2303 <maxim.andreev2303@gmail.com>
2026-09-14 18:26:59 +0300

    Add current day function

--- index.js
+++ index.js
@@ -1,40 +1,17 @@
-const express = require('express');
+function getCurrentDay() {
+  const today = new Date();
 
-const app = express();
+  const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота"
+  ];
 
-const PORT = 8000
-
-const HOST = 'localhost'; 
-
-app.get('/', (req, res)=>{
-    console.log('Server get')
-    res.json('text')
+  console.log(days[today.getDay()]);
 }
-)
-app.get('/Valera', (req, res)=>{
-    res.json({
-        name: "Valera",
-        age: 15,
-        hobby: "ne delyat domashnee zadanye"
-    })
-})
-app.get('/Ilya', (req, res) => {
-    res.json({
-        name: "Ilya",
-        age: "16",
-        hobby: "hobby"
-    })
-})
 
-app.get('/Arina', (req,res)=>{
-    res.json({
-        name: "Arina",
-        age: 16,
-        hobby: "12345678"
-    })
-})
-
-app.listen(PORT, HOST, ()=>{
-    console.log(`Сервер запущен на http://${HOST}:${PORT}`)
-})
-
+getCurrentDay();
